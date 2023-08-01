968895987c81dfa6fc8ec10fd292755c4a463283
DanderTaes <73030752+DanderTaes@users.noreply.github.com>
2023-08-01 13:19:47 +0200

    PythonFix

--- python/calculadora.py
+++ python/calculadora.py
@@ -3,7 +3,7 @@ class Operate:
     def __init__(self): 
         self.base = 6
 
-    def decimal_manager_to_seximal(self, num): # This Python function converts a decimal number to its equivalent in the "seximal" number system (base 6), handling both the integer and decimal parts.
+    def decimal_manager_to_seximal(self, num): # converts a decimal number to its equivalent in the "seximal" number system (base 6), handling both the integer and decimal parts.
         # Manage symbol
         if num < 0:
             num= abs(num)
@@ -12,68 +12,58 @@ class Operate:
             neg = False
         
         # manage floating point numbers
-        if type(num) == float and not num.is_integer(): # test if it's not x.0
+        if type(num) == float:
             decimal_numbers = []
             decimal_numbers = str(num).split(".") # split units and decimals
             units = int(decimal_numbers[0])
-            
+
             # to convert decimals we multiply them by 6^n n been the number of decimals to convert it to a "int" seximal value in base 10, we convert the whole part into seximal and for the decimals we repeat the proceess
-            processing_seximal = round(float(f".{decimal_numbers[1]}")*6**len(str(decimal_numbers[1])), 4)
-            # we do a first iteration and if it's not a whole number we loop
+            processing_seximal = round(float(f".{decimal_numbers[1]}")*self.base**len(str(decimal_numbers[1])), 4)
+            # we do a first iteration and if it's not a whole number we loop 
             if float(processing_seximal).is_integer():
                 whole_decimals = str(self.conversor_to_seximal(int(processing_seximal)))
             else:
                 whole_decimals = ""
-            while not float(processing_seximal).is_integer(): # maximum 8 decimal places to round later
+
+            while not float(processing_seximal).is_integer(): # maximum 8 decimal places to round later --> rounding in seximal was too hard... TODO
                 whole, part = str(processing_seximal).split(".")
+
+                if int(whole) < self.base:   # if in seximal the number is one digit long we add a 0
+                    whole_decimals = whole_decimals + "0"
                 whole_decimals = whole_decimals + str(self.conversor_to_seximal(int(whole)))
-                processing_seximal = round(float(f".{part}")*6**len(str(part)), 4) # the whole part will be converted to seximal and the other will be passed again
-                print(whole_decimals, whole, part)
-                if len(whole_decimals) > 8:
+                processing_seximal = round(float(f".{part}")*self.base**len(str(part)), 4) # the whole part will be converted to seximal and the other will be passed again
+                # print(whole_decimals, whole, part)
+                if len(whole_decimals) >= 8:
                     break
 
-            seximals = int(whole_decimals)
-
-            print("FINAL:", seximals)
-        elif type(num) == float and num.is_integer():
-            num = int(num)
-            units = num
-        else:
-            units = num
-        units = self.conversor_to_seximal(units)
-        try:
+            seximals = whole_decimals
+            units = self.conversor_to_seximal(units)
             num = float(f"{units}.{seximals}")
-        except UnboundLocalError: # if there's no decimals
-            num = units
+
+        else: # if there's no decimals
+            num = self.conversor_to_seximal(num)
         if neg:
             num *= -1
         return num
 
-    def seximal_manager_to_decimal(self, num): # This Python function converts a number in the "seximal" number system (base 6) to its decimal equivalent, handling both the integer and decimal parts.
-        if num < 0:
+    def seximal_manager_to_decimal(self, num): # converts a number in the "seximal" number system (base 6) to its decimal equivalent, handling both the integer and decimal parts.
+        if num < 0: # manage symbol
             num= abs(num)
             neg = True 
         else:
             neg = False
 
-        # TODO: Arreglar decimales al reves
-
         if type(num) == float:
-            seximal_numbers = []
             seximal_numbers = str(num).split(".")
             units = int(seximal_numbers[0])
             digits = len(str(seximal_numbers[1]))
             seximal_numbers[1] = self.conversor_to_decimal(seximal_numbers[1])
-            decimals = round(seximal_numbers[1]/6**digits, 6) # ROUNDS TO 6 DECIMAL PLACES
-              
-        else:
-            units = num
-        units = self.conversor_to_decimal(units)
-        try:
-            
+            decimals = round(seximal_numbers[1]/6**digits, 8) # ROUNDS TO 8 DECIMAL PLACES 
+            # TODO: LOOP --> Update: without the loop it seems to work really well
+            units = self.conversor_to_decimal(units)
             num = units + decimals
-        except UnboundLocalError:
-            num = units
+        else:
+            num = self.conversor_to_decimal(num)
         if neg:
             num *= -1
         return num
@@ -89,10 +79,10 @@ class Operate:
         return num
 
     def conversor_to_decimal(self, num): #:)
-        listS = [int(a) for a in str(num)]
-        listS.reverse()
+        list_seximal = [int(a) for a in str(num)]
+        list_seximal.reverse()
         result = 0
-        for i, number in enumerate(listS):
+        for i, number in enumerate(list_seximal):
             result += number * (self.base**i)        
         return result
 
@@ -166,8 +156,11 @@ class Operate:
             return num
 
 
+        
+
+
 if __name__ == "__main__":
-    number1 = 0.1320543205432# int(input("Number 1: "))
+    number1 = 13.13205432
     op = Operate()
     result = op.seximal_manager_to_decimal(number1)
     if type(result) == int or type(result) == float:
